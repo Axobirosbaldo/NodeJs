bd1afc87fac59fcae9a7976697b60d8cc829a4d1
Pedro Oliveira <letter.pedro@gmail.com>
2018-11-20 01:55:21 -0200

    Adicionando variável global à requisição do middleware
    Ao adicionar uma variável dentro do middleware, todas as requisições que o executam, irão enxergar a variável

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const logMiddleware = (req, res, next) => {
     console.log(
         `HOST: ${req.headers.host} | URL: ${req.url} | METHOD: ${req.method}`
     );
+    
+    req.appName = "GoNode";
 
     return next();
 };
@@ -13,7 +15,7 @@ const logMiddleware = (req, res, next) => {
 app.use(logMiddleware);
 
 app.get('/', (req, res) => {
-    return res.send(`Bem-vindo, ${req.query.name}`);
+    return res.send(`Bem-vindo ao ${req.appName}, ${req.query.name}`);
 });
 
 app.get('/login', (req, res) => {
